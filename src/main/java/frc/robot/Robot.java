d452b5429d1ca4eaa2c78eed75e679a94d9ac2c5
Pete Alderman <peter.alderman@mccardinals.org>
2026-03-13 21:22:26 -0400

    Comment out USB camera and disable Phoenix logging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,26 +1,14 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.UsbCamera;
+//import edu.wpi.first.cameraserver.CameraServer;   //uncomment if we use cameras
+//import edu.wpi.first.cscore.UsbCamera;  //uncomment if we use cameras
 //import edu.wpi.first.math.controller.ElevatorFeedforward;
 //import edu.wpi.first.net.PortForwarder;
+import com.ctre.phoenix6.SignalLogger;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
-
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
@@ -30,23 +18,19 @@ public class Robot extends TimedRobot {
   private RobotContainer m_robotContainer;
 
  
-  
-
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
   @Override
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
+    SignalLogger.stop();   // Force disable Phoenix logging
+    SignalLogger.enableAutoLogging(false);   // Force disable Phoenix logging
     m_robotContainer = new RobotContainer();
 
    //CameraServer.startAutomaticCapture();
     //  for (int port = 5800; port <= 5809; port++) {
 		//  PortForwarder.add(port, "limelight.local", port);
 		//  }
-
+/**  <-- uncomment this if we are going to use USB camera
      m_visionThread1 = new Thread(
 				() -> {
 					 //Get the UsbCamera from CameraServer
@@ -61,6 +45,8 @@ public class Robot extends TimedRobot {
 				});
 		m_visionThread1.setDaemon(true);
 		m_visionThread1.start();
+ */
+
     DriverStation.silenceJoystickConnectionWarning(true);
 
 
